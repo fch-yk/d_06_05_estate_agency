5f906d2e1a09b9ba7e83795f6f271786950f922b
yk <yefim_korshever@mail.ru>
2022-10-30 16:47:45 +0300

    Use the register decorator

--- property/admin.py
+++ property/admin.py
@@ -8,6 +8,7 @@ class OwnersInline(admin.TabularInline):
     raw_id_fields = ['owner', ]
 
 
+@admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address',)
     readonly_fields = ('id', 'created_at',)
@@ -25,18 +26,15 @@ class FlatAdmin(admin.ModelAdmin):
     inlines = [OwnersInline]
 
 
+@admin.register(Complaint)
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ('flat',)
     list_display = ('id', 'user', 'flat',)
 
 
+@admin.register(Owner)
 class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ('flats',)
     list_display = ('id', 'name', 'pure_phone',)
     readonly_fields = ('id',)
     ordering = ('id',)
-
-
-admin.site.register(Flat, FlatAdmin)
-admin.site.register(Complaint, ComplaintAdmin)
-admin.site.register(Owner, OwnerAdmin)
